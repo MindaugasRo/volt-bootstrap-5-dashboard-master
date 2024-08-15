9300767b06acd5d1183a4e408d66ccc247e61893
MindaugasRo <mindaugas.rocys@live.com>
2024-08-15 16:50:05 +0300

    chore: Add API endpoints for contacts in apiEndpoints.js

---
+++ src/assets/js/apiEndpoints.js
@@ -0,0 +1,15 @@
+const API_BASE_URL = 'https://localhost:8085/api';
+
+export const ENDPOINTS = {
+    GET_CONTACTS: `${API_BASE_URL}/contacts`,
+
+    //Get Contacts ALL
+    GET_ALL_CONTACTS: () => `${API_BASE_URL}/contacts/all`,
+
+    // Get contact byID
+    GET_CONTACTS_BY_ID: (contactId) => `${API_BASE_URL}/contacts/id/${contactId}`,
+
+    // Delete contact byID
+    DELETE_CONTACT: (contactId) => `${API_BASE_URL}/contacts/delete/${contactId}`,
+    
+};
